bd51882b0d07c8aeac8b3600681545927fd5a73e
nico <80792499+W9K@users.noreply.github.com>
2021-11-13 17:56:16 +0100

    Update DHB.lua

--- DHB.lua
+++ DHB.lua
@@ -1,1 +1,26 @@
-print"ur mom"
+coroutine.wrap(function()
+
+    local PLR = game:GetService("Players").LocalPlayer
+    local HRP = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+
+    HRP.CFrame = CFrame.new(-869.63232421875, -38.302879333496, -586.50354003906) + Vector3.new(0, 4, 0)
+
+    coroutine.wrap(function()
+        repeat
+            for i, v in pairs(game.Workspace.Ignored.Drop:GetChildren()) do
+                    if v.Name == "MoneyDrop" then
+                        v:Destroy()
+                    end
+                end
+            wait(5)
+            until false
+    end)()
+
+    -- Respawn TP?
+    coroutine.wrap(function()
+        PLR.CharacterAdded:Connect(function()
+            HRP.CFrame = CFrame.new(-869.63232421875, -38.302879333496, -586.50354003906) + Vector3.new(0, 4, 0)
+        end)
+    end)()
+    
+end)()
