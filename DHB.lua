dc8dac1aad43a880dbdec43e68ab583cd6abc342
nico <80792499+W9K@users.noreply.github.com>
2021-11-30 23:34:14 +0100

    Update DHB.lua

--- DHB.lua
+++ DHB.lua
@@ -1,25 +1,28 @@
-local vers = "b1.9"
+repeat 
+    wait() 
+until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+
+local v = 1
 local PLR = game:GetService("Players").LocalPlayer
-local HRP = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+local HRP = PLR.Character.HumanoidRootPart
+
+HRP.CFrame = CFrame.new(-869.63232421875, -38.302879333496, -586.50354003906) + Vector3.new(0, 4, 0)
 
 coroutine.wrap(function()
-    print(vers)
-    HRP.CFrame = CFrame.new(-869.63232421875, -38.302879333496, -586.50354003906) + Vector3.new(0, 4, 0)
-
-    coroutine.wrap(function()
-        repeat
-            for i, v in pairs(game.Workspace.Ignored.Drop:GetChildren()) do
-                    if v.Name == "MoneyDrop" then
-                        v:Destroy()
-                    end
-                end
-            wait(5)
-            until false
-    end)()
-
-    coroutine.wrap(function()
-        PLR.CharacterAdded:Connect(function(char)
-            char:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(-870, -38, -586) + Vector3.new(0, 5, 0)
-        end)
-    end)()
+	coroutine.wrap(function()
+		repeat
+			for i, v in pairs(game.Workspace.Ignored.Drop:GetChildren()) do
+					if v.Name == "MoneyDrop" then
+						v:Destroy()
+					end
+				end
+			wait(5)
+			until false
+	end)()
+	
+	coroutine.wrap(function()
+		PLR.CharacterAdded:Connect(function(char)
+			char:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(-870, -38, -586) + Vector3.new(0, 5, 0)
+		end)
+	end)()
 end)()
